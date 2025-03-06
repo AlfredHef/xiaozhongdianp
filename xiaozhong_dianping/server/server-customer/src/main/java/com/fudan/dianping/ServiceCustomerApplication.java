8c1f83f36b591b4e1c51fd8269eb05e54e59e6a5
xzl <23302010043@m.fudan.edu.cn>
2025-03-06 14:52:14 +0800

    feat(server): 添加 server-customer模块并配置相关依赖
    - 新增 server-customer 模块，用于实现客户相关的服务
    - 添加 MySQL 数据库配置
    -引入 MyBatis-Plus、Druid、PageHelper 等依赖
    - 集成 Spring Cloud Alibaba 组件，支持服务注册和配置
    - 添加 FastJSON、Lombok 等工具类依赖
    - 配置 Swagger 刀片（Knife4j）用于接口文档生成

---
+++ xiaozhong_dianping/server/server-customer/src/main/java/com/fudan/dianping/ServiceCustomerApplication.java
@@ -0,0 +1,12 @@
+package com.fudan.dianping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ServiceCustomerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCustomerApplication.class, args);
+    }
+}
