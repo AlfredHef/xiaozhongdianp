537fcef5067ea3d15ba73a7d8a768e9aaf9f59be
xzl <23302010043@m.fudan.edu.cn>
2025-03-08 22:30:35 +0800

    feat: 新增基础架构和公共组件
    - 新增 BaseException、AccountLockedException、AccountNotFoundException等异常类
    - 新增 AddressBook、Category、Dish等实体类
    - 新增 AliOssProperties、JwtProperties等配置类
    - 新增 AliOssUtil、HttpClientUtil、JwtUtil 等工具类
    - 新增若干常量类和枚举类
    - 新增部分 DTO 和 VO 类

---
+++ xiaozhong_dianping/common/src/main/java/com/fudan/exception/SetmealEnableFailedException.java
@@ -0,0 +1,13 @@
+package com.fudan.exception;
+
+/**
+ * 套餐启用失败异常
+ */
+public class SetmealEnableFailedException extends BaseException {
+
+    public SetmealEnableFailedException(){}
+
+    public SetmealEnableFailedException(String msg){
+        super(msg);
+    }
+}
---
+++ xiaozhong_dianping/common/src/main/java/com/fudan/exception/UserNotLoginException.java
@@ -0,0 +1,12 @@
+package com.fudan.exception;
+
+public class UserNotLoginException extends BaseException {
+
+    public UserNotLoginException() {
+    }
+
+    public UserNotLoginException(String msg) {
+        super(msg);
+    }
+
+}
