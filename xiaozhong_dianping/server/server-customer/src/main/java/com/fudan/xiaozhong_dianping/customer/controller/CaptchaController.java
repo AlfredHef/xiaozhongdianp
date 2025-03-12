ba6e41fb03c999d416f54844a1762c4b56bec92d
Huangenfu <23307110170@m.fudan.edu.cn>
2025-03-12 10:09:36 +0800

    优化验证码相关

--- xiaozhong_dianping/server/server-customer/src/main/java/com/fudan/xiaozhong_dianping/customer/controller/CaptchaController.java
+++ xiaozhong_dianping/server/server-customer/src/main/java/com/fudan/xiaozhong_dianping/customer/controller/CaptchaController.java
@@ -63,7 +63,8 @@ import com.fudan.xiaozhong_dianping.customer.service.CaptchaService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
-import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Base64;
