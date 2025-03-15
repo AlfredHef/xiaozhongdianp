fbffb875eb3b44a34a61d34237985fb2b2be4d19
Suntianyu <23307110043@m.fudan.edu.cn>
2025-03-15 23:21:07 +0800

    登录后跳转到此页面

---
+++ SE_front_end/vue_pj/src/views/HomePage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+    <button @click="changeMessage">点击我</button>
+  </div>
+</template>
+
+<script>
+import { styleType } from 'element-plus/es/components/table-v2/src/common';
+
+export default {
+  data() {
+    return {
+      message:'sty'
+
+    };
+  },
+  methods: {
+    changeMessage() {
+      this.message = '你点击了按钮!';
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
