d0e1beeee73c0f534eaff7169bf82380359e1948
Suntianyu <23307110043@m.fudan.edu.cn>
2025-03-15 23:52:04 +0800

    HomePage相关美化

--- SE_front_end/vue_pj/src/views/HomePage.vue
+++ SE_front_end/vue_pj/src/views/HomePage.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <h1>{{ message }}</h1>
-    <button @click="changeMessage">点击我</button>
+    <button @click="order">点单</button>
+    <button @click="evaluate">评价</button>
   </div>
 </template>
 
@@ -11,13 +12,16 @@ import { styleType } from 'element-plus/es/components/table-v2/src/common';
 export default {
   data() {
     return {
-      message:'sty'
+      message: '欢迎来到软旦餐厅主页！'
 
     };
   },
   methods: {
-    changeMessage() {
-      this.message = '你点击了按钮!';
+    order() {
+      this.message = '点单模式';
+    },
+    evaluate() {
+      this.message = '评价模式';
     }
   }
 };
@@ -35,5 +39,21 @@ button {
   padding: 10px 20px;
   font-size: 16px;
   cursor: pointer;
+  margin: 20px;
+}
+
+/*对 body 元素设置背景样式。
+  使用背景图片 "/Home_BG.jpg" 作为页面的背景。
+  background-size: cover 使背景图片覆盖整个页面。
+  background-position: center 使背景图片居中显示。
+  background-repeat: no-repeat 防止背景图片重复显示。
+  background-attachment: fixed 使背景图片固定，不随页面滚动而滚动。
+*/
+body {
+  background-image: url('/background_homepage.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
 }
 </style>
