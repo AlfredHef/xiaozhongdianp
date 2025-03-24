ad650d5d81fe880bfefa7ef2a7ec3c99787eb6ef
19855826384 <23302010043@m.fudan.edu.cn>
2025-03-24 11:36:46 +0800

    feat(server-customer): 新增店铺相关接口和功能
    - 新增 ShopController 类实现店铺相关接口- 新增 ShopMapper 接口用于店铺数据访问
    - 新增 ShopService 接口和 ShopServiceImpl 实现类，用于店铺业务逻辑处理
    - 更新 compiler.xml 文件，调整 Lombok 注解处理器路径

---
+++ xiaozhong_dianping/server/server-customer/src/main/java/com/fudan/xiaozhong_dianping/shop/controller/ShopController.java
@@ -0,0 +1,7 @@
+package com.fudan.xiaozhong_dianping.shop.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ShopController {
+}
---
+++ xiaozhong_dianping/server/server-customer/src/main/java/com/fudan/xiaozhong_dianping/shop/service/impl/ShopServiceImpl.java
@@ -0,0 +1,8 @@
+package com.fudan.xiaozhong_dianping.shop.service.impl;
+
+import com.fudan.xiaozhong_dianping.shop.service.ShopService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShopServiceImpl implements ShopService {
+}
