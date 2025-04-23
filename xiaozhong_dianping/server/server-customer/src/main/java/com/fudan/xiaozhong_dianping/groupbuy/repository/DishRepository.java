0d1486a402b00c7992cda6df8631bf4da88d0ef9
xzl <23302010043@m.fudan.edu.cn>
2025-04-24 01:12:02 +0800

    feat(server-customer): 新增优惠券相关功能并优化订单服务- 新增用户领取优惠券功能 - 新增用户可用优惠券查询功能 - 新增最大折扣优惠券查询功能 - 优化订单创建流程，支持优惠券折扣 - 修复新人优惠券相关逻辑 - 优化数据库查询效率，使用 MyBatis 注解

--- xiaozhong_dianping/server/server-customer/src/main/java/com/fudan/xiaozhong_dianping/groupbuy/repository/DishRepository.java
+++ xiaozhong_dianping/server/server-customer/src/main/java/com/fudan/xiaozhong_dianping/groupbuy/repository/DishRepository.java
@@ -1,6 +1,7 @@
 package com.fudan.xiaozhong_dianping.groupbuy.repository;
 
 import com.fudan.xiaozhong_dianping.groupbuy.entity.Dish;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -17,5 +18,6 @@ public interface DishRepository extends JpaRepository<Dish, Integer> {
      * @param shopId 商家ID
      * @return 菜品列表
      */
+    @Select("select * from dish where shop_id=#{shopId}")
     List<Dish> findByShopId(Integer shopId);
 } 
